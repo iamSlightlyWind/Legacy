d6ae31673c2a25701dbb1390028a7e4d64681e07
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-02-25 03:31:06 +0700

    asm2

---
+++ PRO192x/Assignments/asm2/GradeStudent.java
@@ -0,0 +1,6 @@
+public class GradeStudent{
+    public static void main(String[] args){
+        System.out.println("This program reads exam/homework scores and reports your overall course grade.");
+        
+    }
+}
