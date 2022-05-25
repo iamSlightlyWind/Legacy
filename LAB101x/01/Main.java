e0a7b728d631b87aaaf5965f8926cdf48d157a38
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-05-25 20:10:57 +0700

    Update lab101x.01: language

--- LAB101x/01/Main.java
+++ LAB101x/01/Main.java
@@ -17,38 +17,38 @@ public class Main {
 
     public static void createStudentProfile() {
         System.out.println("---------------------");
-        System.out.println("Creating a student profile");
+        System.out.println("Tạo hồ sơ học sinh: ");
 
-        System.out.print("Student name: ");
+        System.out.print("Tên hhọc sinh: ");
         String name = scan.nextLine();
 
-        System.out.print("Select semester: ");
+        System.out.print("Học kỳ: ");
         int semester = scan.nextInt();
 
-        System.out.println("Subjects list: ");
+        System.out.println("Danh sách môn học: ");
         System.out.println("1. Java\n2. .Net\n3. C / C ++");
-        System.out.print("Chosen subject: ");
+        System.out.print("Môn học đăng ký: ");
         int subject = scan.nextInt();
 
         student.add(new Student(student.size(), semester, subject, name));
 
-        System.out.println("Student profile saved: " + student.get(student.size() - 1).toString());
+        System.out.println("Hồ sơ học sinh được lưu: " + student.get(student.size() - 1).toString());
     }
 
     public static void studentSearch() {
         System.out.println("---------------------");
-        System.out.println("Searching for a student profile");
+        System.out.println("Tìm kiếm hồ sơ học sinh: ");
 
         ArrayList<Student> temp = student;
         temp.sort((o1, o2) -> o1.getName().compareTo(o2.getName()));
 
-        System.out.print("Search using name: ");
+        System.out.print("Tìm kiếm học sinh bằng tên: ");
         String name = scan.nextLine();
 
         for (int i = 0; i < temp.size(); i++) {
             if (temp.get(i).getStatus()) {
                 if (temp.get(i).getName().contains(name)) {
-                    System.out.println("Student found: " + temp.get(i).toString());
+                    System.out.println("Tìm thấy học sinh: " + temp.get(i).toString());
                 }
             }
         }
@@ -56,10 +56,19 @@ public class Main {
 
     public static void editStudentProfile() {
         System.out.println("---------------------");
-        System.out.println("Creating a student profile");
+        System.out.println("Chỉnh sửa hồ sơ học sinh: ");
 
         System.out.print("Enter student ID: ");
         int id = scan.nextInt();
+
+        for (int i = 0; i < student.size(); i++) {
+            if(student.get(i).getStatus()){
+                if(student.get(i).getID() == id){
+                    System.out.println("Tìm thấy học sinh: " + student.get(i).toString());
+                    System.out.print("Bạn muốn cập nhật (U) hay xóa (D) học sinh: ");
+                }
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -71,9 +80,8 @@ public class Main {
         // studentSearch();
         // createStudentProfile();
         // System.out.println();
+
         studentSearch();
         System.out.println();
-
-        // ArrayList<Student> temp = student;
     }
 }
