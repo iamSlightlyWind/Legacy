5eeb502680e2612b53af453d16a63752a50e9e2a
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-09-07 15:29:52 +0700

    LAB101x: 13: since they asked for it

--- LAB101x/13/Main.java
+++ LAB101x/13/Main.java
@@ -1,15 +1,23 @@
 public class Main {
+    static int count = 1;
+
+    static void printFib(int num1, int num2) {
+        System.out.print(num1 + " ");
+
+        int next = num1 + num2;
+        num1 = num2;
+        num2 = next;
+
+        if(count == 45) System.exit(0);
+        count++;
+        printFib(num1, num2);
+    }
+
     public static void main(String[] args) {
 
         int num1 = 0, num2 = 1;
         System.out.println("The 45 sequence fibonacci: ");
 
-        for (int i = 0; i < 45; i++) {
-            System.out.print(num1 + " ");
-
-            int next = num1 + num2;
-            num1 = num2;
-            num2 = next;
-        }
+        printFib(num1, num2);
     }
 }
