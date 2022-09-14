fd06f6d8589db20e17042d47c071e128079cfaab
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-09-14 08:38:17 +0700

    LAB101x: 02: better input, search ignores cases

--- LAB101x/02/Main.java
+++ LAB101x/02/Main.java
@@ -15,7 +15,7 @@ public class Main {
         System.out.println(
                 " (Vui lòng chọn 1 để tạo Ứng viên có kinh nghiệm, 2 để tạo ứng viên Fresher, 3 cho ứng viên thực tập sinh, 4 để tìm kiếm và 5 để thoát chương trình).");
 
-        switch (scan.nextInt()) {
+        switch (Integer.parseInt(scan.nextLine())) {
             case 1:
                 createCandidateProfile(1);
                 break;
@@ -58,151 +58,6 @@ public class Main {
                 System.out.println(i.getInfo("name") + "\n");
             }
         });
-    }
-
-    public static boolean check(String attempt, String argument) {
-        switch (argument) {
-            case "birthYear":
-                try {
-                    if (attempt.length() == 4) {
-                        Integer.parseInt(attempt);
-                    } else
-                        throw new NumberFormatException("");
-                } catch (NumberFormatException ex) {
-                    System.out.println("Input not recognized: birth year must be a 4 letter number");
-                    return false;
-                }
-
-                return true;
-
-            case "gradYear":
-                try {
-                    if (attempt.length() == 4) {
-                        Integer.parseInt(attempt);
-                    } else
-                        throw new NumberFormatException("");
-                } catch (NumberFormatException ex) {
-                    System.out.println("Input not recognized: graduation year must be a 4 letter number");
-                    return false;
-                }
-
-                return true;
-
-            case "phoneNumber":
-                try {
-                    if (attempt.length() >= 10) {
-                        Long.parseLong(attempt);
-                    } else
-                        throw new NumberFormatException("");
-                } catch (NumberFormatException ex) {
-                    System.out.println(
-                            "Input not recognized: phone number must only includes alphanumeric characters which length equal to or more than 10");
-                    return false;
-                }
-
-                return true;
-
-            case "emailAddress":
-                int count = 0, splitLocation = 0;
-
-                for (int i = 0; i < attempt.length(); i++) {
-                    if (attempt.charAt(i) == '@') {
-                        count++;
-                        splitLocation = ++i;
-                        if (splitLocation == 1) {
-                            System.out.println("Input not recognized: username in email address cannot be empty");
-                            return false;
-                        }
-
-                        String username = attempt.substring(0, splitLocation - 1);
-
-                        if (username.charAt(username.length() - 1) == '.' || username.charAt(0) == '.') {
-                            System.out.println(
-                                    "Input not recognized: \".\" cannot appear at the start nor the end of the username");
-                            return false;
-                        }
-                    }
-
-                    if (count > 1) {
-                        System.out
-                                .println("Input not recognized: email address must have one occurrence of \"@\"");
-                        return false;
-                    }
-                }
-
-                if (count == 0) {
-                    System.out.println("Input not recognized: email address must have one occurrence of \"@\"");
-                    return false;
-                }
-                count = 0;
-
-                String domain = attempt.substring(splitLocation, attempt.length());
-
-                if (domain.length() < 3) {
-                    System.out.println("Input not recognized: domain length must be equal to or more than 3");
-                    return false;
-                }
-
-                if (domain.charAt(domain.length() - 1) == '.' || domain.charAt(0) == '.') {
-                    System.out.println(
-                            "Input not recognized: \".\" cannot appear at the start nor the end of the domain");
-                    return false;
-                }
-
-                for (int i = 0; i < domain.length(); i++) {
-                    if (domain.charAt(i) == '.') {
-                        return true;
-                    }
-                }
-
-                if (count == 0) {
-                    System.out
-                            .println("Input not recognized: domain address must have at least one occurrence of \".\"");
-                    return false;
-                }
-
-                return true;
-
-            case "expYears":
-                try {
-                    if (Integer.parseInt(attempt) >= 0 && Integer.parseInt(attempt) <= 100) {
-                        return true;
-                    } else
-                        throw new NumberFormatException("");
-                } catch (NumberFormatException ex) {
-                    System.out.println("Input not recognized: phone number must be a 10 letter number");
-                    return false;
-                }
-
-            case "gradRank":
-                if (attempt.equals("Xuat sac") || attempt.equals("Tot") || attempt.equals("Kha")
-                        || attempt.equals("Kem")) {
-                    return true;
-                } else {
-                    System.out.println(
-                            "Input not recognized: rank of graduation must either be one of the following values:");
-                    System.out.println("Xuat sac");
-                    System.out.println("Tot");
-                    System.out.println("Kha");
-                    System.out.println("Kem");
-                    return false;
-                }
-
-            case "semester":
-                try {
-                    if (attempt.length() > 0) {
-                        Integer.parseInt(attempt);
-                    } else
-                        throw new NumberFormatException("");
-                } catch (NumberFormatException ex) {
-                    System.out.println("Input not recognized: semester must only includes alphanumeric characters");
-                    return false;
-                }
-
-                return true;
-        }
-
-        return false;
     }
 
     public static void createCandidateProfile(int candidateType) {
@@ -313,6 +168,180 @@ public class Main {
 
     }
 
+    public static void searchCandidate() {
+        System.out.print("Nhập tên ứng viên (Họ hoặc tên): ");
+        String searchFor = scan.nextLine();
+        System.out.print("Loại đầu vào của ứng viên: ");
+        String candType = scan.next();
+        scan.nextLine();
+        System.out.println();
+
+        cands.forEach(i -> {
+            if (i.getInfo("type").equals(candType)) {
+                if (i.getInfo("name").toLowerCase().contains(searchFor.toLowerCase())) {
+                    System.out.println(i.getInfo("detail"));
+                }
+            }
+        });
+
+    }
+
+    public static boolean check(String attempt, String argument) {
+        switch (argument) {
+            case "birthYear":
+                try {
+                    if (attempt.length() == 4) {
+                        Integer.parseInt(attempt);
+                    } else
+                        throw new NumberFormatException("");
+                } catch (NumberFormatException ex) {
+                    System.out.println("Input not recognized: birth year must be a 4 letter number");
+                    return false;
+                }
+
+                return true;
+
+            case "gradYear":
+                try {
+                    if (attempt.length() == 4) {
+                        Integer.parseInt(attempt);
+                    } else
+                        throw new NumberFormatException("");
+                } catch (NumberFormatException ex) {
+                    System.out.println("Input not recognized: graduation year must be a 4 letter number");
+                    return false;
+                }
+
+                return true;
+
+            case "phoneNumber":
+                try {
+                    if (attempt.length() >= 10) {
+                        Long.parseLong(attempt);
+                    } else
+                        throw new NumberFormatException("");
+                } catch (NumberFormatException ex) {
+                    System.out.println(
+                            "Input not recognized: phone number must only includes alphanumeric characters which length equal to or more than 10");
+                    return false;
+                }
+
+                return true;
+
+            case "emailAddress":
+                int count = 0, splitLocation = 0;
+
+                for (int i = 0; i < attempt.length(); i++) {
+                    if (attempt.charAt(i) == '@') {
+                        count++;
+                        splitLocation = ++i;
+                        if (splitLocation == 1) {
+                            System.out.println("Input not recognized: username in email address cannot be empty");
+                            return false;
+                        }
+
+                        String username = attempt.substring(0, splitLocation - 1);
+
+                        if (username.charAt(username.length() - 1) == '.' || username.charAt(0) == '.') {
+                            System.out.println(
+                                    "Input not recognized: \".\" cannot appear at the start nor the end of the username");
+                            return false;
+                        }
+                    }
+
+                    if (count > 1) {
+                        System.out
+                                .println("Input not recognized: email address must have one occurrence of \"@\"");
+                        return false;
+                    }
+                }
+
+                for (int i = 0, atCount = 0; i < attempt.length(); i++) {
+                    if (attempt.charAt(i) == '@') {
+                        atCount++;
+                    }
+                    if (atCount > 1) {
+                        System.out
+                                .println("Input not recognized: only one occurrence of @ is allowed in email address");
+                        return false;
+                    }
+                }
+
+                if (count == 0) {
+                    System.out.println("Input not recognized: email address must have one occurrence of \"@\"");
+                    return false;
+                }
+                count = 0;
+
+                String domain = attempt.substring(splitLocation, attempt.length());
+
+                if (domain.length() < 3) {
+                    System.out.println("Input not recognized: domain length must be equal to or more than 3");
+                    return false;
+                }
+
+                if (domain.charAt(domain.length() - 1) == '.' || domain.charAt(0) == '.') {
+                    System.out.println(
+                            "Input not recognized: \".\" cannot appear at the start nor the end of the domain");
+                    return false;
+                }
+
+                for (int i = 0; i < domain.length(); i++) {
+                    if (domain.charAt(i) == '.') {
+                        return true;
+                    }
+                }
+
+                if (count == 0) {
+                    System.out
+                            .println("Input not recognized: domain address must have at least one occurrence of \".\"");
+                    return false;
+                }
+
+                return true;
+
+            case "expYears":
+                try {
+                    if (Integer.parseInt(attempt) >= 0 && Integer.parseInt(attempt) <= 100) {
+                        return true;
+                    } else
+                        throw new NumberFormatException("");
+                } catch (NumberFormatException ex) {
+                    System.out.println("Input not recognized: phone number must be a 10 letter number");
+                    return false;
+                }
+
+            case "gradRank":
+                if (attempt.equals("Xuat sac") || attempt.equals("Tot") || attempt.equals("Kha")
+                        || attempt.equals("Kem")) {
+                    return true;
+                } else {
+                    System.out.println(
+                            "Input not recognized: rank of graduation must either be one of the following values:");
+                    System.out.println("Xuat sac");
+                    System.out.println("Tot");
+                    System.out.println("Kha");
+                    System.out.println("Kem");
+                    return false;
+                }
+
+            case "semester":
+                try {
+                    if (attempt.length() > 0) {
+                        Integer.parseInt(attempt);
+                    } else
+                        throw new NumberFormatException("");
+                } catch (NumberFormatException ex) {
+                    System.out.println("Input not recognized: semester must only includes alphanumeric characters");
+                    return false;
+                }
+
+                return true;
+        }
+
+        return false;
+    }
+
     public static void createData() {
         ////////////////////////////////////////// Experienced
         cands.add(new Experienced());
@@ -379,23 +408,6 @@ public class Main {
                 "0911 Jairo Circles");
         cands.get(cands.size() - 1).setID(cands.size() - 1);
         ((Intern) cands.get(cands.size() - 1)).setIntern("Literature", "7", "Barnard College");
-    }
-
-    public static void searchCandidate() {
-        System.out.print("Nhập tên ứng viên (Họ hoặc tên): ");
-        String searchFor = scan.next();
-        System.out.print("Loại đầu vào của ứng viên: ");
-        String candType = scan.next();
-        System.out.println();
-
-        cands.forEach(i -> {
-            if (i.getInfo("type").equals(candType)) {
-                if (i.getInfo("name").contains(searchFor)) {
-                    System.out.println(i.getInfo("detail"));
-                }
-            }
-        });
-
     }
 
     public static void main(String[] args) {
