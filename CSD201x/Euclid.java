49c06336629908f770bf80ac6a61d6025ae22db8
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2022-09-27 19:46:57 +0700

    CSD201x: Part 1

---
+++ CSD201x/Euclid.java
@@ -0,0 +1,13 @@
+public class Euclid {
+    public static int calc(int a, int b) {
+        int result = -1;
+
+        while (result != 0) {
+            result = a % b;
+            a = b;
+            b = result;
+        }
+
+        return a;
+    }
+}
---
+++ CSD201x/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    static int[] myInt = { 8, 3, 7, 2, 9, 6 };
+
+    public static void main(String[] args) {
+        // System.out.println(Euclid.calc(88, 3);
+        Bubble.sort(myInt);
+
+        System.out.println();
+    }
+}
