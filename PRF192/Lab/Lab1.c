209d0b6bff99d072cb4617082f1f25e2fa269906
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2023-01-05 17:26:53 +0700

    PRF192: Lab 01

---
+++ PRF192/Lab/Lab1.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("A");
+    printf("\nB C");
+    printf("\nD E F");
+    printf("\nG H I J");
+    printf("\nK L M N 0");
+
+    printf("\n\n");
+
+    printf("*        *\n");
+    printf("**      **\n");
+    printf("***    ***\n");
+    printf("****  ****\n");
+    printf("**********\n");
+    printf("**********\n");
+    printf("****  ****\n");
+    printf("***    ***\n");
+    printf("**      **\n");
+    printf("*        *\n");
+
+    printf("\n");
+
+    printf("    *    \n");
+    printf("   ***   \n");
+    printf("  *****  \n");
+    printf(" ******* \n");
+    printf("*********\n");
+
+    printf("\n");
+
+    printf("    *    \n");
+    printf("   * *   \n");
+    printf("  * * *  \n");
+    printf(" * * * * \n");
+    printf("* * * * *\n");
+    printf("* * * * *\n");
+    printf(" * * * * \n");
+    printf("  * * *  \n");
+    printf("   * *   \n");
+    printf("    *    \n");
+
+    printf("\n");
+
+    printf("**********\n");
+    printf("****  ****\n");
+    printf("***    ***\n");
+    printf("**      **\n");
+    printf("*        *\n");
+    printf("*        *\n");
+    printf("**      **\n");
+    printf("***    ***\n");
+    printf("****  ****\n");
+    printf("**********\n");
+
+    printf("\n");
+
+    printf("* * * * *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("*       *\n");
+    printf("* * * * *\n");
+
+    printf("\n");
+
+    printf("* * * * * \n");
+    printf(" * * * * *\n");
+    printf("* * * * * \n");
+    printf(" * * * * *\n");
+    printf("* * * * * \n");
+
+    printf("\n");
+
+    printf("\n          '__'");
+    printf("\n          (oo) ");
+    printf("\n  *========\\/  ");
+    printf("\n / || %%%%%% ||  ");
+    printf("\n*  ||-----||  ");
+    printf("\n   \"\"     \"\"    ");
+}
