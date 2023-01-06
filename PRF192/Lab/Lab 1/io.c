99edd93d66d605cb66aa94c61e0bd1e46f23fac3
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2023-01-06 07:47:24 +0700

    PRF192: Lab 01

--- PRF192/Lab/Lab 1/io.c
+++ PRF192/Lab/Lab 1/io.c
@@ -1,10 +1,34 @@
 #include <stdio.h>
 
-int main(){
-    printf("\nGood day!");
-    printf("\nGood /\\ day!");
-    printf("\nHe shared his \"wisdom\" with me");
-    printf("\nHello\n     world!");
-    printf("\nHello\n\tworld!");
-    
+int main()
+{
+    printf("Good day!");
+    printf("\n\n");
+
+    printf("Good /\\ day!");
+    printf("\n\n");
+
+    printf("He shared his \"wisdom\" with me");
+    printf("\n\n");
+
+    printf("Hello\n     world!");
+    printf("\n\n");
+
+    printf("Hello\n\tworld!");
+    printf("\n\n");
+
+    printf("How/tare/tyou?\nI\tam\tOK");
+    printf("\n\n");
+
+    printf("How/tare/tyou?");
+    printf("\n\nI\tam\tOK");
+    printf("\n\n");
+
+    printf("How/tare/tyou?\nI\tam\tOK");
+    printf("\n\n");
+
+    printf("Something has gone crazy\a");
+    printf("\n\n");
+
+    printf("Bank interest is 10%% in the year 2001.");
 }
