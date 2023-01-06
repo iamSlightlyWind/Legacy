a419d309f622691bf112a790e4f37c9e5c4a6416
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2023-01-06 08:49:19 +0700

    PRF192: Lab 01

--- PRF192/Lab/Lab 1/Lab1.c
+++ PRF192/Lab/Lab 1/Lab1.c
@@ -16,7 +16,7 @@ int main()
     printf("* * * *     * * * *\n");
     printf("* * * * * * * * * *\n");
     printf("* * * * * * * * * *\n");
-    printf("* * * *     *  * * *\n");
+    printf("* * * *     * * * *\n");
     printf("* * *         * * *\n");
     printf("* *             * *\n");
     printf("*                 *\n");
