abf6080b32ed5c15db041d7e6efaf9f59ef5c038
iamSlightlyWind <70873548+iamSlightlyWind@users.noreply.github.com>
2023-10-29 12:36:19 +0700

    IOT102: Project Optimized functions, added alarm time (not alarm per se), tested servo.

--- IOT102/IOT.ino
+++ IOT102/IOT.ino
@@ -1,56 +1,57 @@
 #include <LiquidCrystal.h>
+#include <Servo.h>
+
 int rs = 2, en = 3, d4 = 4, d5 = 5, d6 = 6, d7 = 7;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-int servo = 8;
+Servo servo;
+
 int buttonNext = 9, buttonPrev = 10;
 int potentiometer = A0;
 
-String hour = "0", minute = "0";
+String currentHour = "00", currentMinute = "00";
+String alarmHour = "00", alarmMinute = "00";
 
 int current = 0;
 
 void setup()
 {
-    pinMode(servo, OUTPUT);
     pinMode(buttonNext, INPUT);
     pinMode(buttonPrev, INPUT);
 
+    servo.attach(8);
+
     lcd.begin(16, 2);
     Serial.begin(9600);
+    delay(100);
 }
 
 void loop()
 {
     process();
-
-    //delay(100);
 }
 
 void process()
 {
     getInput();
     updateTime();
+    spinServo();
     print();
 }
 
-int blinkDuration = 100;
-int printDuration = 300;
+void spinServo()
+{
+    servo.write(map(analogRead(potentiometer), 0, 1023, 180, 0));
+}
 
-void blink(){
-    if(current == 0){
-        delay(printDuration);
-        lcd.setCursor(0, 1);
-        lcd.print("  ");
-        delay(blinkDuration);
-    }
+void blink()
+{
+    int startLocation[] = {0, 3, 6, 9};
 
-    if(current == 1){
-        delay(printDuration);
-        lcd.setCursor(3, 0);
-        lcd.print("  ");
-        delay(blinkDuration);
-    }
+    delay(300);
+    lcd.setCursor(startLocation[current], 1);
+    lcd.print("  ");
+    delay(100);
 }
 
 void print()
@@ -66,8 +67,14 @@ void print()
     lcd.setCursor(3, 0);
     lcd.print(current);
 
+    lcd.setCursor(6, 0);
+    lcd.print(String(analogRead(potentiometer)));
+
     lcd.setCursor(0, 1);
-    lcd.print(String(hour) + ":" + String(minute));
+    lcd.print(String(currentHour) + ":" + String(currentMinute));
+
+    lcd.setCursor(6, 1);
+    lcd.print(String(alarmHour) + ":" + String(alarmMinute));
 
     blink();
 }
@@ -76,14 +83,15 @@ void getInput()
 {
     if (digitalRead(buttonNext) == HIGH)
     {
-        if (current == 0)
+        if (current < 3)
         {
             current++;
         }
     }
+
     if (digitalRead(buttonPrev) == HIGH)
     {
-        if (current == 1)
+        if (current > 0)
         {
             current--;
         }
@@ -92,8 +100,25 @@ void getInput()
 
 void updateTime()
 {
-    hour = format(updateValue("hour"));
-    minute = format(updateValue("minute"));
+    if (current == 0)
+    {
+        currentHour = format(updateValue("hour"));
+    }
+
+    if (current == 1)
+    {
+        currentMinute = format(updateValue("minute"));
+    }
+
+    if (current == 2)
+    {
+        alarmHour = format(updateValue("hour"));
+    }
+
+    if (current == 3)
+    {
+        alarmMinute = format(updateValue("minute"));
+    }
 }
 
 String format(String value)
@@ -102,8 +127,8 @@ String format(String value)
     {
         return "0" + String(value);
     }
-    else
-        return String(value);
+
+    return String(value);
 }
 
 String updateValue(String choice)
@@ -111,22 +136,8 @@ String updateValue(String choice)
     double value = analogRead(potentiometer);
 
     if (choice == "hour")
-    {
-        if (current == 0)
-        {
-            return String(int(value / 1024 * 24));
-        }
-        else
-            return hour;
-    }
+        return String(map(analogRead(potentiometer), 0, 1023, 0, 24));
 
     if (choice == "minute")
-    {
-        if (current == 1)
-        {
-            return String(int(value / 1024 * 60));
-        }
-        else
-            return minute;
-    }
+        return String(map(analogRead(potentiometer), 0, 1023, 0, 60));
 }
